ed16de81b0f79226425edac42516b2d516657f3f
Advait177013 <72642229+Advait177013@users.noreply.github.com>
2022-04-28 15:48:29 +0530

    Add files via upload

--- Karaoke.py
+++ Karaoke.py
@@ -1,25 +1,16 @@
 import os
 import time
 from playsound import playsound
-import Lyrics
+import SongPlayers
+import LyricPlayers
 from multiprocessing import Process
 
-#if __name__ == "__main__":
-#def sound():
-#    playsound('OceanMan.mp3')
-
 if __name__ == "__main__":
-    #p = Process(target=sound)
-    #p.start()
-    #input("test : ")
-    #p.terminate()
-    #input("wow : ")
     while(1):
         os.system('cls')
         print("Welcome to Improv Karaoke\nWe have a selection of songs available for you")
         extensions=[".wav", ".mp3"]
         file_list=[]
-        print("pid is "+str(os.getpid()))
         for path, dirs, files in os.walk('.'):
             for filename in files:
                 if any(ext in filename for ext in extensions):
@@ -34,12 +25,23 @@ if __name__ == "__main__":
         sgname = file_list[x]
         print("Now playing : "+sgname)
         if(sgname=='Exitlude.mp3'):
-            p1=Process(target=Lyrics.playExitlude)
-            p1.start()
-            input("\nPress any key to terminate\n")
-            p1.terminate()
+            p1=Process(target=SongPlayers.playExitlude)
+            p2=Process(target=LyricPlayers.playExitlude)
         elif(sgname=='OceanMan.mp3'):
-            p1=Process(target=Lyrics.playOcean)
-            p1.start()
-            input("\nPress any key to terminate\n")
-            p1.terminate()
+            p1=Process(target=SongPlayers.playOcean)
+            p2=Process(target=LyricPlayers.playOcean)
+        elif(sgname=='RickRoll.mp3'):
+            p1=Process(target=SongPlayers.playRick)
+            p2=Process(target=LyricPlayers.playRick)
+        elif(sgname=='LoseYourself.mp3'):
+            p1=Process(target=SongPlayers.playLoseYourself)
+            p2=Process(target=LyricPlayers.playLoseYourself)
+        elif(sgname=='CountryRoads.mp3'):
+            p1=Process(target=SongPlayers.playCountry)
+            p2=Process(target=LyricPlayers.playCountry)
+
+        p1.start()
+        p2.start()
+        input("\nPress any key to terminate\n")
+        p1.terminate()
+        p2.terminate()
---
+++ SongPlayers.py
@@ -0,0 +1,16 @@
+from playsound import playsound
+
+def playExitlude():
+    playsound('Exitlude.mp3', True)
+
+def playOcean():
+    playsound('OceanMan.mp3', True)
+
+def playCountry():
+    playsound('CountryRoads.mp3', True)
+
+def playRick():
+    playsound('RickRoll.mp3', True)
+
+def playLoseYourself():
+    playsound('LoseYourself.mp3', True)
